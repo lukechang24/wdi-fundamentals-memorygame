121e495e452f5ecd07eb5df7dd2caf7149c73c3e
Luke Chang <lukechang244@gmail.com>
2019-08-02 11:37:33 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,84 @@
-var cardOne = "queen";
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+var cardsInPlay = [];
+document.querySelector("#QoD").onclick = function(e) {
+	if(e.target.className === "flipped") {
+		flipCardsBack(e.target);
+	} else {
+		flipCard(e.target);
+		cardsInPlay.push("Queen");
+		console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+		checkForPair(e.target);
+	}
+}
+document.querySelector("#QoH").onclick = function(e) {
+	if(e.target.className === "flipped") {
+		flipCardsBack(e.target);
+	}
+	else {
+		flipCard(e.target);
+		cardsInPlay.push("Queen");
+		console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+		checkForPair(e.target);
+	}}
+document.querySelector("#KoD").onclick = function(e) {
+	if(e.target.className === "flipped") {
+		flipCardsBack(e.target);
+	}
+	else {
+		flipCard(e.target);
+		cardsInPlay.push("King");
+		console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+		checkForPair(e.target);
+	}
+}
+document.querySelector("#KoH").onclick = function(e) {
+	if(e.target.className === "flipped") {
+		flipCardsBack(e.target);
+	}
+	else {
+		flipCard(e.target);
+		cardsInPlay.push("King");
+		console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+		checkForPair(e.target);
+	}
+}
+function flipCard(selectedCard) {
+	if(selectedCard.id === "QoD") {
+		selectedCard.src = "images/queen-of-diamonds.png";
+		selectedCard.className = "flipped";
+	} else if(selectedCard.id === "QoH") {
+		selectedCard.src = "images/queen-of-hearts.png";
+		selectedCard.className = "flipped";
+	} else if(selectedCard.id === "KoD") {
+		selectedCard.src = "images/king-of-diamonds.png";
+		selectedCard.className = "flipped";
+	} else if(selectedCard.id === "KoH") {
+		selectedCard.src = "images/king-of-hearts.png";
+		selectedCard.className = "flipped";
+	}
+}
+function flipCardsBack(selectedCard) {
+	selectedCard.src = "images/back.png";
+	selectedCard.className = "facedown";
+}
+
+function checkForPair(card) {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			card.parentNode.removeChild(document.querySelector(".flipped"));
+			card.parentNode.removeChild(document.querySelector(".flipped"));
+			console.log("You found a pair!");
+			cardsInPlay = [];
+
+		}
+		else {
+			console.log("You did not find a pair.")
+			flipCardsBack(document.querySelector(".flipped"));
+			flipCardsBack(document.querySelector(".flipped"));
+			cardsInPlay = [];
+		}
+	}
+}
+
+document.getElementById("button").onclick = function() {
+	checkForPair();
+}
